600e8ac7bdf6dd48433efca66105cc953bb168f1
rehab-megahed123 <rehabmegahed241@gmail.com>
2025-04-04 14:19:19 +0200

    final update in interceptor

--- src/app/core/Interceptors/myloading.interceptor.ts
+++ src/app/core/Interceptors/myloading.interceptor.ts
@@ -1,12 +1,27 @@
 import { HttpEventType, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { finalize, tap } from 'rxjs';
+import { catchError, finalize, of, tap } from 'rxjs';
 
 export const myloadingInterceptor: HttpInterceptorFn = (req, next) => {
-const spiner=inject(NgxSpinnerService)
-spiner.show()
-  return next(req).pipe(finalize(()=>{
-spiner.hide()
-  }))
+  const spinner = inject(NgxSpinnerService);
+
+  spinner.show();
+
+  return next(req).pipe(
+    tap({
+      next: (event: any) => {
+        if (event?.status && event.status !== 200 && event.status !== 201) {
+          alert('There was a problem with the response from the server.');
+        }
+      }
+    }),
+    catchError((error) => {
+      alert('An error occurred while processing the request.');
+      return of(error); 
+    }),
+    finalize(() => {
+      spinner.hide();
+    })
+  );
 };
